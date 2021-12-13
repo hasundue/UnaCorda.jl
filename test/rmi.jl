94c2087a8fae327ff77c5f25acbef337567301a5
Shun Ueda <hasundue@gmail.com>
2021-12-13 17:28:06 +0900

    Mock Richards' equation

--- test/rmi.jl
+++ test/rmi.jl
@@ -3,39 +3,38 @@ using Gridap
 using GridapODEs.ODETools
 using GridapODEs.TransientFETools
 
-f(t) = 1.0
+e = VectorValue(1.0)
 
-u₀(x) = 0.0
+u(x,t) = 1.0
+u(t) = x -> u(x,t)
+f(t) = x -> ∂t(u)(x,t) - Δ(u(t))(x) - e⋅∇(u(t))(x)
 
 domain = (0,1)
 cells = (10,)
 model = CartesianDiscreteModel(domain, cells)
 
-δ = DiracDelta{0}(model, tags=1)
-
-order = 2
+order = 1
 V = TestFESpace(model,
 		ReferenceFE(lagrangian, Float64, order),
-		conformity=:H1)
-U = TransientTrialFESpace(V)
+		conformity=:H1,
+		dirichlet_tags=1)
+U = TransientTrialFESpace(V, u)
 
 degree = 2 * order
 Ω = Triangulation(model)
 dΩ = Measure(Ω, degree)
 
-a(u,v) = ∫(∇(v)⋅∇(u))dΩ
-b(v,t) = δ(f(t)*v)
+a(t,u,v) = ∫(∇(v)⋅∇(u))dΩ + ∫((e⊙u)⋅∇(v))dΩ
+b(t,v) = ∫(v⋅f(t))dΩ
+m(t,ut,v) = ∫(ut⋅v)dΩ
 
-res(t,u,v) = a(u,v) + ∫(∂t(u)*v)dΩ - b(v,t)
-jac(t,u,du,v) = a(du,v)
-jac_t(t,u,dut,v) = ∫(dut*v)dΩ
-
-op = TransientFEOperator(res, jac, jac_t, U, V)
+op = TransientAffineFEOperator(m, a, b, U, V)
 
 t₀ = 0.0
 t₁ = 1.0
 δt = 0.1
 
+u₀(x) = 0.0
 U₀ = U(0.0)
 uh₀ = interpolate_everywhere(u₀, U₀)
 
@@ -44,11 +43,10 @@ ls = LUSolver()
 ode_solver = ThetaMethod(ls, δt, θ)
 
 sol_t = solve(ode_solver, op, uh₀, t₀, t₁)
-
 using Plots
 using Printf
 plt = plot()
 for (uh_tn, tn) in sol_t
-	plot!(plt, uh_tn.free_values[1:9], label = @sprintf "t = %1.1f" tn)
+	plot!(plt, uh_tn.free_values[1:10], label = @sprintf "t = %1.1f" tn)
 end
 plot(plt)
